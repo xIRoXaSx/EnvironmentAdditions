7ca8bb9e78f60ba504657e3a4914132133727eff
RoXaS <log2.me@outlook.com>
2021-11-23 17:50:45 +0100

    Added reload functionality Added biome placeholder Removed unused method(s) Removed debugging messages

--- src/main/java/net/lizardnetwork/biomeevents/BiomeEvents.java
+++ src/main/java/net/lizardnetwork/biomeevents/BiomeEvents.java
@@ -4,13 +4,21 @@ import net.lizardnetwork.biomeevents.configuration.Config;
 import net.lizardnetwork.biomeevents.helper.Parser;
 import net.lizardnetwork.biomeevents.models.BiomeModel;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
-public class BiomeEvents extends JavaPlugin implements Listener {
-    private final Config config = new Config(this);
+public class BiomeEvents extends JavaPlugin implements Listener, CommandExecutor {
+    private final String pluginName = this.getDescription().getName();
     private static int positionChecksInTicks = -1;
+    private Config config = new Config(this);
+    private BukkitTask locationCheckerTask;
     static List<BiomeModel> biomeModels;
 
     @Override
@@ -25,12 +33,73 @@ public class BiomeEvents extends JavaPlugin implements Listener {
             return;
         }
 
-        biomeModels = config.getBiomeConfigs();
+        start();
+        Bukkit.getLogger().info("Enabled " + pluginName + " within " + getFormattedTime(startTime));
+    }
 
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (args.length == 0)
+            return true;
+
+        if (args[0].equalsIgnoreCase("reload")) {
+            if (!sender.hasPermission(pluginName.toLowerCase() + ".admin"))
+                return false;
+
+            String elapsedTime = reload();
+
+            if (sender instanceof Player)
+                Bukkit.getLogger().info(elapsedTime);
+
+            sender.sendMessage(elapsedTime);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * The start logic of this plugin
+     */
+    private void start() {
+        biomeModels = config.getBiomeConfigs();
         Bukkit.getLogger().info("Found " + biomeModels.size() + " biomes in the config!");
         positionChecksInTicks = Parser.parse(config.getConfigProperty("BiomeEvents.Settings.PositionChecksInTicks"), 20);
-        LocationChecker locationChecker = new LocationChecker(this, config.getConfigProperty("BiomeEvents.Settings.PapiBiomePlaceholder"));
-        locationChecker.initializeTimeDrivenSystem();
+        locationCheckerTask = startLocationChecker();
+    }
+
+    /**
+     * Reload the configuration and restart the BukkitTask
+     * @return String - The time taken to reload
+     */
+    private String reload() {
+        long startTime = System.nanoTime();
+
+        locationCheckerTask.cancel();
+        config = new Config(this);
+        start();
+
+        return Parser.getColorizedText("&aReloaded " + pluginName + "&f within " + getFormattedTime(startTime));
+    }
+
+    /**
+     * Get the elapsed time formatted in ms or s depending on the length
+     * @param startTime long - The start time
+     * @return String - Formatted string link '10ms'
+     */
+    private String getFormattedTime(long startTime) {
+        long elapsed = (System.nanoTime() - startTime) / 1000000;
+        return (elapsed <= 1000 ? elapsed + "ms": elapsed / 1000 + "s");
+    }
+
+    /**
+     * Start the LocationChecker BukkitTask
+     * @return BukkitTask - Running BukkitTask of LocationChecker
+     */
+    private BukkitTask startLocationChecker() {
+        return new LocationChecker(this, config.getConfigProperty("BiomeEvents.Settings.PapiBiomePlaceholder"))
+            .initializeTimeDrivenSystem();
     }
 
     /**
@@ -39,14 +108,6 @@ public class BiomeEvents extends JavaPlugin implements Listener {
      */
     public static Integer getPositionChecksInTicks() {
         return positionChecksInTicks;
-    }
-
-    /**
-     * Get the current Config instance
-     * @return Config - The current config
-     */
-    Config getCurrentConfig() {
-        return config;
     }
 
     /**
--- src/main/java/net/lizardnetwork/biomeevents/external/PlaceholderApiHook.java
+++ src/main/java/net/lizardnetwork/biomeevents/external/PlaceholderApiHook.java
@@ -3,39 +3,62 @@ package net.lizardnetwork.biomeevents.external;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.lizardnetwork.biomeevents.enums.Dependency;
 import net.lizardnetwork.biomeevents.helper.DependencyChecker;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PlaceholderApiHook implements Listener {
     private final boolean isEnabled;
+    private boolean forceDefaultCheck = false;
 
+    /**
+     * Replace placeholder by either internal checks or PlaceholderAPI if installed
+     */
     public PlaceholderApiHook() {
         DependencyChecker dependencyChecker = new DependencyChecker();
         Dependency dependency = dependencyChecker.getEnabledDependencies();
         isEnabled = dependency.equals(Dependency.PlaceholderAPI);
     }
 
+    /**
+     * Replace placeholder by either internal checks or PlaceholderAPI if installed
+     * @param forceDefaultCheck Boolean - Whether to default to internal placeholder check or not
+     */
+    public PlaceholderApiHook(boolean forceDefaultCheck) {
+        DependencyChecker dependencyChecker = new DependencyChecker();
+        Dependency dependency = dependencyChecker.getEnabledDependencies();
+        this.isEnabled = dependency.equals(Dependency.PlaceholderAPI);
+        this.forceDefaultCheck = forceDefaultCheck;
+    }
+
     public String getPlaceholder(Player player, String text) {
-        if (isEnabled) {
-            Bukkit.getLogger().info("Using PAPI!");
+        if (isEnabled && !forceDefaultCheck) {
             return PlaceholderAPI.setPlaceholders(player, text);
         } else {
             Map<String, String> replacements = Map.of(
-                "world", player.getWorld().getName(),
-                "player_biome", player.getLocation().getBlock().getBiome().name(),
-                "x", String.valueOf(player.getLocation().getBlockX()),
-                "player_x", String.valueOf(player.getLocation().getBlockX()),
-                "y", String.valueOf(player.getLocation().getBlockY()),
-                "player_y", String.valueOf(player.getLocation().getBlockY()),
-                "z", String.valueOf(player.getLocation().getBlockZ()),
-                "player_z", String.valueOf(player.getLocation().getBlockY()),
                 "player", player.getName(),
-                "player_name", player.getName()
+                "world", player.getWorld().getName(),
+                "biome", player.getLocation().getBlock().getBiome().name(),
+                "x", String.valueOf(player.getLocation().getBlockX()),
+                "y", String.valueOf(player.getLocation().getBlockY()),
+                "z", String.valueOf(player.getLocation().getBlockZ())
             );
 
-            Placeholder placeholder = new Placeholder(text, replacements);
+            Map<String, String> playerReplacements = Map.of(
+                "player_name", player.getName(),
+                "player_world", player.getWorld().getName(),
+                "player_biome", player.getLocation().getBlock().getBiome().name(),
+                "player_x", String.valueOf(player.getLocation().getBlockX()),
+                "player_y", String.valueOf(player.getLocation().getBlockY()),
+                "player_z", String.valueOf(player.getLocation().getBlockY())
+            );
+
+            Map<String, String> allReplacements = new HashMap<>();
+            allReplacements.putAll(replacements);
+            allReplacements.putAll(playerReplacements);
+
+            Placeholder placeholder = new Placeholder(text, allReplacements);
             return placeholder.replace();
         }
     }
